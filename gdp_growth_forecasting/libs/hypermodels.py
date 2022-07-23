d88ccf25b60f08805c8868d6d1102c5642bb1e06
valegrei <victoralegre@uni.pe>
2022-07-22 20:36:00 -0500

    Modificando modelos

--- gdp_growth_forecasting/libs/hypermodels.py
+++ gdp_growth_forecasting/libs/hypermodels.py
@@ -4,27 +4,7 @@ from kerastuner import HyperModel
 
 
 
-class CNNHyperModel(HyperModel):
 
-    def __init__(self, n_features, n_steps_out, metrics = ['mae','mse','mape'], name=None, tunable=True):
-        super().__init__(name=name, tunable=tunable)
-        self.n_features = n_features
-        self.n_steps_out = n_steps_out
-        self.metrics = metrics
-
-    def build(self, hp):
-        # Parametrizamos tamano de kernel, nro de kernels, nro de nodos en capa MLP, y ratio de aprendizaje
-        hp_time_steps = hp.Int('steps_in',4,20,step=2)
-        tam_kernel = hp.Int('tam_kernel',2,3,step=1)
-        nro_kernels = hp.Int('nro_kernels',32,160,step=32)
-        nro_nodos = hp.Int('nro_nodos',32,160,step=32)
-        hp_ratio_aprendizaje = hp.Choice('ratio_aprendizaje', values=[1e-2, 1e-3, 1e-4])
-        
-        return build_cnn(hp_time_steps,self.n_features,self.n_steps_out,nro_kernels,tam_kernel,nro_nodos,hp_ratio_aprendizaje,self.metrics)
-
-    def fit(self, hp, model,x,y, **kwargs):
-        x_s,y_s = shift_data(x,y,hp.get('steps_in'))
-        return model.fit(x=x_s,y=y_s,**kwargs)
 
 class LSTMHyperModel(HyperModel):
 
--- gdp_growth_forecasting/libs/models.py
+++ gdp_growth_forecasting/libs/models.py
@@ -5,13 +5,15 @@ from keras.layers.convolutional import Conv1D
 from keras.layers.convolutional import MaxPooling1D
 from keras.layers import LSTM
 from keras.layers import GRU
+from keras.layers import Dropout
 from keras.layers import RepeatVector
 from keras.layers import TimeDistributed
 from tensorflow.keras.optimizers import Adam
 
 
 def build_mlp(n_steps_in: int, n_features: int, n_steps_out: int, nodes: int,
-              layers: int, learning_rate: float, activation=None, metrics = None):
+              layers: int, dropout : float, learning_rate: float, activation=None,
+              metrics = None):
     '''
     Construye un Perceptron Multicapa (MLP)
 
@@ -19,18 +21,28 @@ def build_mlp(n_steps_in: int, n_features: int, n_steps_out: int, nodes: int,
     ----------
     n_steps_in : int
         Pasos de tiempo a pasado de entrada
+
     n_features : int
         Numero de caracteristicas de la entrada
+
     n_steps_out : int
         Pasos de tiempo a futuro de salida
+
     nodes : int
         Numero de nodos por capa oculta
+
     layers : int
         Numero de capas ocultas
+
+    dropout : float
+        Ratio de dropout
+
     learning_rate : float
         Ratio de aprendizaje
+
     activation : 
         Funcion de activacion.
+
     metrics : Any
         Lista de Metricas
 
@@ -43,14 +55,18 @@ def build_mlp(n_steps_in: int, n_features: int, n_steps_out: int, nodes: int,
     model.add(Flatten(input_shape=(n_steps_in, n_features)))
     for i in range(layers):
         model.add(Dense(nodes, activation=activation))
+    model.add(Dropout(dropout))
     model.add(Dense(n_steps_out))
     model.compile(optimizer=Adam(learning_rate=learning_rate),
                   loss='mse', metrics=metrics)
     return model
 
 
-def build_cnn(n_steps_in: int, n_features: int, n_steps_out: int, kernels: int,
-              kernel_size: int, nodes: int, learning_rate: float, activation=None, metrics = None):
+def build_cnn(n_steps_in: int, n_features: int, n_steps_out: int, conv1_kernels: int,
+              conv1_kernel_size: int, conv1_dropout : float, conv2_kernels: int,
+              conv2_kernel_size: int, conv2_dropout : float, dense_nodes: int, 
+              dense_layers : int, dense_dropout : float, learning_rate: float, 
+              conv_activation = None, dense_activation = None, metrics = None):
     '''
     Construye una Red Convolucional (CNN)
 
@@ -58,20 +74,49 @@ def build_cnn(n_steps_in: int, n_features: int, n_steps_out: int, kernels: int,
     ----------
     n_steps_in : int
         Pasos de tiempo a pasado de entrada
+
     n_features : int
         Numero de caracteristicas de la entrada
+
     n_steps_out : int
         Pasos de tiempo a futuro de salida
-    kernels : int
-        Numero de kernels
-    kernel_size : int
-        Tamano de kernel
-    nodes : int
-        Numero de nodos por capa oculta
+
+    conv1_kernels : int
+        Numero de kernels para Conv 1
+
+    conv1_kernel_size : int
+        Tamano de kernel para Conv 1
+
+    conv1_dropout : float
+        Ratio de dropout para Conv 1
+
+    conv2_kernels : int
+        Numero de kernels para Conv 2
+
+    conv2_kernel_size : int
+        Tamano de kernel para Conv 2
+
+    conv2_dropout : float
+        Ratio de dropout para Conv 2
+
+    dense_nodes : int
+        Numero de nodos por capa densa
+
+    dense_layers : int
+        Numero de capas densas
+    
+    dense_dropout : float
+        Ratio de dropout para capas densas
+
     learning_rate : float
         Ratio de aprendizaje
-    activation : 
-        Funcion de activacion.
+
+    conv_activation : 
+        Funcion de activacion para Conv
+
+    dense_activation : 
+        Funcion de activacion para Dense
+
     metrics : Any
         Lista de Metricas
 
@@ -81,13 +126,28 @@ def build_cnn(n_steps_in: int, n_features: int, n_steps_out: int, kernels: int,
         Modelo CNN construido
     '''
     model = Sequential()
+    # Conv 1
     model.add(Conv1D(
-        filters=kernels,
-        kernel_size=kernel_size,
-        activation=activation, input_shape=(n_steps_in, n_features)))
-    model.add(MaxPooling1D(pool_size=kernel_size, padding='same'))
+        filters=conv1_kernels,
+        kernel_size=conv1_kernel_size,
+        activation=conv_activation, 
+        padding = 'same',
+        input_shape=(n_steps_in, n_features)))
+    model.add(MaxPooling1D(pool_size=conv1_kernel_size, padding='same'))
+    model.add(Dropout(conv1_dropout))
+    # Conv 2
+    model.add(Conv1D(
+        filters=conv2_kernels,
+        kernel_size=conv2_kernel_size,
+        activation=conv_activation, 
+        padding = 'same'))
+    model.add(MaxPooling1D(pool_size=conv2_kernel_size, padding='same'))
+    model.add(Dropout(conv2_dropout))
+    # Dense
     model.add(Flatten())
-    model.add(Dense(nodes, activation=activation))
+    for i in dense_layers:
+        model.add(Dense(dense_nodes, activation=dense_activation))
+    model.add(Dropout(dense_dropout))
     model.add(Dense(n_steps_out))
     model.compile(optimizer=Adam(learning_rate=learning_rate),
                   loss='mse', metrics=metrics)
@@ -147,8 +207,10 @@ def build_gru(n_steps_in: int, n_features: int, n_steps_out: int, cells: int, la
     ----------
     n_steps_in : int
         Pasos de tiempo a pasado de entrada
+
     n_features : int
         Numero de caracteristicas de la entrada
+        
     n_steps_out : int
         Pasos de tiempo a futuro de salida
     cells : int
